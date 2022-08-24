7dddb7a3e38a8d2463d43f624230cf50a8b1c610
sathiyapriya27 <sathiyap7700@gmail.com>
2022-08-24 20:38:50 +0530

    program

---
+++ cprog/Number.c
@@ -0,0 +1,19 @@
+/*
+ * Number.c
+ *
+ *  Created on: 24-Aug-2022
+ *      Author: selva-temp
+ */
+
+#include<stdio.h>
+void Number()
+{
+
+		for (int i=0; i<10; i++){
+			printf("\n");
+			for (int j=0; j<10; j++){
+						printf("%d",j);
+			}
+		}
+}
+
---
+++ cprog/matrix.c
@@ -0,0 +1,35 @@
+/*
+ * matrix.c
+ *
+ *  Created on: 22-Aug-2022
+ *      Author: selva-temp
+ */
+
+#include<stdio.h>
+void matrix()
+{
+	//int k=3,m=2;
+    int var1 [3][2]={{0,1},{2,3},{4,5}};
+	for (int k=0; k<3; k++){
+		for(int m=0; m<2; m++){
+			printf("var1[%d][%d] =%d\n", k, m, var1[k][m]);
+		}
+	}
+	//int i=3,j=2;
+	int var2 [3][2]={{9,1},{7,2},{4,3}};
+		for (int i=0; i<3; i++){
+			for(int j=0; j<2; j++){
+				printf("var2[%d][%d] = %d\n", i, j, var2 [i][j]);
+			}
+		}
+		//int var3[3][2];
+	for (int a=0; a<3; a++){
+		for(int q=0; q<2; q++){
+			printf("var1 [%d][%d]+var2 [%d][%d]=var3[%d]\n", a,q, a,q, var1 [a][q] += var2[a][q] );
+		}
+	}
+
+
+	}
+
+
