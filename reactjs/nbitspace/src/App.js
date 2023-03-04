81f7dec0e2ce2c0c7bb9809f8894e368543fd087
nbitspace <nbitspace@gmail.com>
2023-03-04 08:37:34 +0530

    react js teaching

--- reactjs/nbitspace/src/App.js
+++ reactjs/nbitspace/src/App.js
@@ -1,15 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import Test, {Tt} from './myfolder/Test'
+import { Container } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
+    <Container maxWidth="xl">
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div id='dummy'></div>
-        <Test />
-        <Tt />
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12 }}>
+          <Grid item xs={2} sm={4} md={4} lg={3} key={1}> 
+            <Test myName="Nila" myAge='18' link ="https://engadget.com" linkName = 'Engadget'/>
+            <Tt />
+          </Grid>
+          <Grid item xs={2} sm={4} md={4} lg={3} key={1}>
+            <Test 
+            myName="Nilan" 
+            myAge='20'
+            link ="https://google.com" 
+            linkName = 'Google'
+            />
+          </Grid>
+          <Grid item xs={2} sm={4} md={4} lg={3} key={1}>
+            <Test myName="Venkadesh" myAge='21' link ="https://openai.com" linkName = 'Open AI' />
+          </Grid>
+          <Grid item xs={2} sm={4} md={4} lg={3} key={1}>
+            <Test myName="Akash" myAge='20' link ="https://fb.com" linkName = 'FB'/>
+          </Grid>
+        </Grid>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,8 +41,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
+    </Container>
+
   );
 }
 
--- reactjs/nbitspace/src/myfolder/Test.js
+++ reactjs/nbitspace/src/myfolder/Test.js
@@ -1,32 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import Check from './Check';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Button } from '@mui/material';
 
-function Test() {
-    var myName = "Nilan"
+function Test(props) {
+    //var myName = "Nilan"
     //var number = 4+10;
-    var something = 0
-    const [number, setNumber] = useState(4 + 10)
+    const [num, setNumber] = useState(4 + 10)
 
     //myName = (myName != "Nila" )? "Not Nila" : "Nila";
     // var element = <div>Oh Maria</div>;
     // var elem = ReactDOM.createRoot(document.getElementById('dummy'));
     // elem.render(<Test />);
     
-    const handleChange = (event) => setNumber(parseInt(event.target.value))
+    const handleChange = event => setNumber(parseInt(event.target.value))
+
+
+    const handleChange4 = (event) => setNumber(parseInt(event.target.value))
+
+    const handleChange1 = (event) => {
+        setNumber(parseInt(event.target.value))
+    }
+
+    const handleChange2 = function (event) {
+        setNumber(parseInt(event.target.value))
+    }
+
+    function handleChange3 (event) {
+        setNumber(parseInt(event.target.value))
+    }
+
     
     return (
         <div className="Test">
-            HI {myName}, we are in Nbit Space #{number + 6}. Your name is {(myName !== "Nila" )? "Not Nila" : "Nila"}.
-            <input type='text' name='val1' onChange={handleChange}/>
+           
+            
+            HI {props.myName}, we are in Nbit Space #{num + 6}. 
+            Your name is {(props.myName !== "Nila" )? "Not Nila" : "Nila"}.
+            
+            <input type='text' name='name1' onChange={handleChange}/>
+            
+            Hi my age is {props.myAge}.
+            <div>
+                <a href={props.link}>{props.linkName}</a> 
+            <br/><br/>
+            <center><Check /> </center>
+            <br/>
+            </div>
+     
         </div>
     );
 }
 
 export function Tt() {
+    const [x, setX] = useState('Revision Class 2');
+    const [y, setY] = useState(0);
+
+    function showTimestamp(event) {
+        let d = Date.now();
+        setX(d);
+        setY(parseInt(d/1000));
+        console.log("func");
+    }
+
     return (
         <React.Fragment>
-            <div>Revision Class 1</div>
-            <div>Revision class 2</div>
+            <div className="tt">{y}</div>
+            <div >{x}</div>
+            <Button onClick={showTimestamp}>Current Timestamp</Button>
+            <br/>
         </React.Fragment>
     );
 }
